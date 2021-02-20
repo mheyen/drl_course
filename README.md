73eb40d49501165ec161c25f62503b52c10f7a2d
Thomas <thomasklein18@gmail.com>
2021-02-20 14:51:27 +0100

    install nomkl because conda sucks, run basic mnist example

--- README.md
+++ README.md
@@ -2,4 +2,12 @@
 Block-course on Deep Reinforcement Learning at Osnabrück University, Spring 2021
 
 # Setup
-To setup the conda environment, just run ./setup.sh.
+First, download and install Conda and initialize your shell by running `conda init --all`.
+To setup the conda environment, run `./setup.sh`, which will create an environment inside this project.
+Activate this environment by running `conda activate ./drl_env`. (To deactivate, run `conda deactivate`)
+You can test the installation by running `python3 src/sanitycheck.py` which will try to import tf and print the version.
+
+# Notes on Tensorflow-Version and nomkl
+So far, conda only supports tf up to 2.0.0 on macOS, which is why this verison is hard-coded in the environment-yml.
+To see if this has changed, go to https://anaconda.org/anaconda/tensorflow.
+nomkl is installed for similar reasons, if we switch at some point, we can get rid of that as well.
--- src/sanitycheck.py
+++ src/sanitycheck.py
@@ -1,8 +1,36 @@
 import numpy as np 
 import tensorflow as tf 
 
+def try_mnist():
+
+    print("Training a basic model on mnist")
+
+    mnist = tf.keras.datasets.mnist
+
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Dense(10)
+        ])
+
+    loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+    model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+    model.fit(x_train, y_train, epochs=5)
+
+    model.evaluate(x_test,  y_test, verbose=2)
+
 if __name__ == "__main__":
-    
+
     print("Checking if Tensorflow has been installed correctly.")
 
     print("Tensorflow version: ", tf.__version__)
+
+    try_mnist()
